bf8f93a4acd9e5de28bade4794f73545fcfe054f
theovgl <contact@theovogel.fr>
2022-03-22 21:35:29 +0100

    feat: color change

--- src/good-old-blackhole.js
+++ src/good-old-blackhole.js
@@ -1,12 +1,18 @@
 chrome.runtime.onMessage.addListener((request) => {
 	if (request.status == "loaded") {
-		var emote = document.getElementsByClassName("emote-bh")[0];
-		var daysLeft = emote.getAttribute("data-original-title");
+		const emote = document.getElementsByClassName("emote-bh")[0];
+		const daysLeft = emote.getAttribute("data-original-title").replace("days left", "");
+		let daysCount = parseInt(daysLeft, 10);
 		var bhDate = document.getElementById("bh-date");
 
-		bhDate.innerText = daysLeft;
-		daysLeft.innerText = ""
+		if (daysCount > 42)
+			bhDate.style.color = "#69db5a";
+		else if (daysCount > 15)
+			bhDate.style.color = "#e89e2e";
+		else
+			bhDate.style.color = "#ff3636";
 
+		bhDate.innerText = daysCount + " days left";
 		console.log(daysLeft);
 	}
 })

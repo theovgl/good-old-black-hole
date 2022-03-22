fedf4168aadd45711c1bbcbbe99ca86888c0d5cb
theovgl <contact@theovogel.fr>
2022-03-22 21:46:46 +0100

    add eslint and fix style errors

--- src/gobh_background.js
+++ src/gobh_background.js
@@ -1,15 +1,15 @@
-let callback = function(details) {
-	chrome.tabs.sendMessage(details.tabId, {status: "loaded"})
-}
+/* eslint-disable no-undef */
+const callback = (details) => {
+  chrome.tabs.sendMessage(details.tabId, { status: 'loaded' });
+};
 
-let filter = {
-	urls: ["*://profile.intra.42.fr/users/*/goals?cursus=*"],
-	types: ["xmlhttprequest"]
-}
+const filter = {
+  urls: ['*://profile.intra.42.fr/users/*/goals?cursus=*'],
+  types: ['xmlhttprequest'],
+};
 
 chrome.webRequest.onCompleted.addListener(
-	callback,
-	filter,
-	[]
+  callback,
+  filter,
+  [],
 );
-

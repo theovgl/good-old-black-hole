e95fdd2103bfd22c9d3fac2304a9a73f98f9e67f
theovgl <contact@theovogel.fr>
2022-03-22 22:58:51 +0100

    refactor: change eslint config

--- src/gobh_background.js
+++ src/gobh_background.js
@@ -1,15 +1,15 @@
 /* eslint-disable no-undef */
 const callback = (details) => {
-  chrome.tabs.sendMessage(details.tabId, { status: 'loaded' });
+	chrome.tabs.sendMessage(details.tabId, { status: 'loaded' });
 };
 
 const filter = {
-  urls: ['*://profile.intra.42.fr/users/*/goals?cursus=*'],
-  types: ['xmlhttprequest'],
+	urls: ['*://profile.intra.42.fr/users/*/goals?cursus=*'],
+	types: ['xmlhttprequest'],
 };
 
 chrome.webRequest.onCompleted.addListener(
-  callback,
-  filter,
-  [],
+	callback,
+	filter,
+	[],
 );
--- src/good-old-blackhole.js
+++ src/good-old-blackhole.js
@@ -1,15 +1,15 @@
 /* eslint-disable no-undef */
 chrome.runtime.onMessage.addListener((request) => {
-  if (request.status === 'loaded') {
-    const emote = document.getElementsByClassName('emote-bh')[0];
-    const daysLeft = emote.getAttribute('data-original-title').replace('days left', '');
-    const daysCount = parseInt(daysLeft, 10);
-    const bhDate = document.getElementById('bh-date');
+	if (request.status === 'loaded') {
+		const emote = document.getElementsByClassName('emote-bh')[0];
+		const daysLeft = emote.getAttribute('data-original-title').replace('days left', '');
+		const daysCount = parseInt(daysLeft, 10);
+		const bhDate = document.getElementById('bh-date');
 
-    if (daysCount > 42) bhDate.style.color = '#69db5a';
-    else if (daysCount > 15) bhDate.style.color = '#e89e2e';
-    else bhDate.style.color = '#ff3636';
+		if (daysCount > 42) bhDate.style.color = '#69db5a';
+		else if (daysCount > 15) bhDate.style.color = '#e89e2e';
+		else bhDate.style.color = '#ff3636';
 
-    bhDate.innerText = `${daysCount} days left`;
-  }
+		bhDate.innerText = `${daysCount} days left`;
+	}
 });
